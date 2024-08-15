2e2881386e45da50caec6129ea066dbdb570e9d7
Igor Dias <igordiasth@gmail.com>
2024-08-15 18:55:21 +0000

    check num assets `rename_assets.py`

---
+++ rename_assets.py
@@ -0,0 +1,17 @@
+import unreal
+
+def rename_assets():
+    # Instances of unreal classes
+    system_lib = unreal.SystemLibrary()
+    editor_util = unreal.EditorUtilityLibrary()
+    string_lib = unreal.StringLibrary()
+
+    # Get the selected assets
+    selected_assets = editor_util.get_selected_assets()
+    num_assets = len(selected_assets)
+    replaced = 0
+
+    unreal.log(f"Selected {num_assets} assets...")
+
+
+rename_assets()
