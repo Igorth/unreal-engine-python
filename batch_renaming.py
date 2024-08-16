67ac958a41d6631a0b3ff09f45b98cb1399b93b0
Igor Dias <igordiasth@gmail.com>
2024-08-16 18:46:39 +0000

    iterate over the assets `batch_renaming.py`

--- batch_renaming.py
+++ batch_renaming.py
@@ -7,7 +7,18 @@ def get_selected_content_browser_assets():
 
 def run():
     selected_assets = get_selected_content_browser_assets()
-    print(selected_assets)
+    print("Using a for each loop")
+    for asset in selected_assets:
+        print(f"Asset: {asset.get_name()}")
+
+    print("\nUsing a list comprehension")
+    asset_names = [asset.get_name() for asset in selected_assets]
+    print(asset_names)
+
+    print("\nUsing a classic for loop")
+    for i in range(len(selected_assets)):
+        asset = selected_assets[i]
+        print(f"Asset: {asset.get_name()}")
 
 
 run()
