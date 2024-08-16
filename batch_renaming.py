07ebc777899c9e244f082f0b8354bee9384fcbc1
Igor Dias <igordiasth@gmail.com>
2024-08-16 19:10:00 +0000

    generate new name for assets `batch_renaming.py`

--- batch_renaming.py
+++ batch_renaming.py
@@ -5,20 +5,38 @@ def get_selected_content_browser_assets():
     selected_assets = editor_utility.get_selected_assets()
     return selected_assets
 
+def generate_new_name_for_asset(asset):
+    # Log to see the type of the asset
+    # print(f"Asset {asset.get_name()} is a {type(asset)}")
+
+    # Is asset a MaterialInstance
+    if isinstance(asset, unreal.MaterialInstance):
+        return "MI_" + asset.get_name()
+    
+    # Is asset a Material
+    if isinstance(asset, unreal.Material):
+        return "M_" + asset.get_name()
+    
+    # Is asset a Texture
+    if isinstance(asset, unreal.Texture):
+        return "T_" + asset.get_name()
+    
+    # Is asset a NiagaraSystem
+    if isinstance(asset, unreal.NiagaraSystem):
+        return "NS_" + asset.get_name()
+    
+    return asset.get_name()
+
 def run():
     selected_assets = get_selected_content_browser_assets()
-    print("Using a for each loop")
-    for asset in selected_assets:
-        print(f"Asset: {asset.get_name()}")
 
-    print("\nUsing a list comprehension")
-    asset_names = [asset.get_name() for asset in selected_assets]
-    print(asset_names)
-
-    print("\nUsing a classic for loop")
     for i in range(len(selected_assets)):
         asset = selected_assets[i]
-        print(f"Asset: {asset.get_name()}")
+
+        old_name = asset.get_name()
+        new_name = generate_new_name_for_asset(asset)
+
+        print(f"Renaming {old_name} to {new_name}")
 
 
 run()
