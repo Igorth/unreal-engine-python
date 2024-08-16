3d1a65f0d6d2e4621d4ba9f8202deb14e5b55c1d
Igor Dias <igordiasth@gmail.com>
2024-08-16 18:28:15 +0000

    add other object `placing_objects.py`

--- placing_objects.py
+++ placing_objects.py
@@ -1,50 +1,36 @@
 import unreal
+import math
 
-def spawn_cube(location=unreal.Vector(), rotation=unreal.Rotator()):
-
+def spawn_cube(location = unreal.Vector(0, 0, 0), rotation = unreal.Rotator(0, 0, 0)):
     # Get the system to control the actors
     editor_actor_subs = unreal.get_editor_subsystem(unreal.EditorActorSubsystem)
-
+    
     # Create a StaticMeshActor
     actor_class = unreal.StaticMeshActor
 
     # Place it in the level
     static_mesh_actor = editor_actor_subs.spawn_actor_from_class(actor_class, location, rotation)
 
-    # Load and add the cube to it
-    static_mesh = unreal.EditorAssetLibrary.load_asset("/Engine/BasicShapes/Cube")
+    # Load and add the mesh (Cube, Cylinder, Sphere)
+    static_mesh = unreal.EditorAssetLibrary.load_asset("/Engine/BasicShapes/Cylinder")
     static_mesh_actor.static_mesh_component.set_static_mesh(static_mesh)
 
-
 def run():
 
-    cube_count = 20
-
-    for i in range(cube_count):
-        circle_x_location = i * 150
-        circle_y_location = 0
-
-        location = unreal.Vector(circle_x_location, circle_y_location, 0)
-        spawn_cube(location)
-
-
-def run():
-
-    cube_count = 20
+    mesh_count = 30
     circle_radius = 1000
     circle_center = unreal.Vector(0, 0, 0)
 
-    for i in range(cube_count):
-        circle_x_location = circle_radius * math.cos(math.radians(i * 360 / cube_count))
-        circle_y_location = circle_radius * math.sin(math.radians(i * 360 / cube_count))
+    for i in range(mesh_count):
+        circle_x_location = circle_radius * math.cos(math.radians(i * 360 / mesh_count))
+        circle_y_location = circle_radius * math.sin(math.radians(i * 360 / mesh_count))
 
         location = unreal.Vector(circle_x_location, circle_y_location, 0)
+        # Cubes Orientation
         location_to_circle_center = location - circle_center
 
         rotation = location_to_circle_center.quaternion().rotator()
 
         spawn_cube(location, rotation)
 
-
-with unreal.ScopedEditorTransaction("Place cubes in a circle") as transaction:
-    run()
+run()
